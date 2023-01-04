5cfca7b7d554b7e945f5779de163c3961c01053d
Yonahey <cherfi.ismael@gmail.comm>
2023-01-04 12:30:46 +0100

    Style Changes, first steps to List item supressions

--- src/App.js
+++ src/App.js
@@ -1,32 +1,90 @@
 // import "./App.css";
 import {React, useState, useEffect} from "react";
-import {getArgonautes} from "./api/axios";
+import {getArgonautes, addArgonautes, deleteArgonautes} from "./api/axios";
 import "./style/main.css";
 
 function App() {
 	const [argonautes, setArgonautes] = useState([]);
+	const [argonauteInput, setArgonauteInput] = useState("");
+	const [serverResponse, setServerResponse] = useState("");
+	const [argonauteList, setArgonauteList] = useState([]);
 
 	useEffect(() => {
 		getArgonautes().then((res) => {
 			setArgonautes(res.data);
 		});
-	}, []);
+
+		setTimeout(() => {
+			setServerResponse("");
+		}, 5000);
+	}, [serverResponse]);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		console.log(argonauteInput);
+		const data = {
+			argonauteName: argonauteInput,
+		};
+
+		addArgonautes(data).then((res) => {
+			setServerResponse(res.data);
+		});
+	};
 
 	const argonautesList = argonautes.map((argonaute) => {
-		return <tr> {argonaute.name}</tr>;
+		return (
+			<tr
+				key={argonaute._id}
+				className=" border border-solid text-center"
+			>
+				<td>{argonaute.name}</td>
+				<td>
+					<button>Suppr</button>
+				</td>
+			</tr>
+		);
 	});
 	return (
-		<div className="App">
-			<h1>Bienvenu</h1>
+		<div>
+			<h1 className="welcomeMessage">Liste des argonautes</h1>
 
-			<table>
-				<thead>
-					<tr>Argonautes:</tr>
-				</thead>
-				<tbody>
-					<tr>{argonautesList}</tr>
-				</tbody>
-			</table>
+			<div className="m-10 argonauteInput">
+				<h3>Ajouter un argonaute : </h3>
+				<form onSubmit={handleSubmit}>
+					<input
+						type="text"
+						className="border border-black  "
+						value={argonauteInput}
+						onChange={(e) => {
+							setArgonauteInput(e.target.value);
+						}}
+					/>
+					<button className="border border-black"> go !</button>
+				</form>
+			</div>
+			<p>
+				{serverResponse === "cette argonaute existe déjà !"
+					? "l'Argonaute existe déjà !"
+					: serverResponse === ""
+					? ""
+					: "Argonaute Crée !"}
+			</p>
+			<div className="  tableContainer flex justify-center mx-auto  ">
+				<div className="flex flex-col">
+					<div className="w-full">
+						<div className="border-b border-gray-200 shadow">
+							<table>
+								<thead className="bg-gray-50">
+									<th className="px-52 py-2 text-xs text-gray-500">
+										Argonautes:
+									</th>
+								</thead>
+								<tbody>{argonautesList}</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
--- src/api/axios.js
+++ src/api/axios.js
@@ -7,3 +7,12 @@ export const apiBaseUrl = axios.create({
 export const getArgonautes = async () => {
 	return apiBaseUrl.get("/getArgonautesList");
 };
+
+export const addArgonautes = async ({argonauteName}) => {
+	console.log(argonauteName);
+	return apiBaseUrl.post("/createArgonaute", {argonauteName});
+};
+
+export const deleteArgonautes = async ({argonauteId}) => {
+	return apiBaseUrl.post("/deleteArgonaute", {argonauteId});
+};
